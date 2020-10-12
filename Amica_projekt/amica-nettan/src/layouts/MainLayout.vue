a378ba0fc455a9070df2cf5c47ba75b2de0095e8
davidmorck <david.morck@abbindustrigymnasium.se>
2020-10-12 16:26:25 +0200

    primero

---
+++ Amica_projekt/amica-nettan/src/layouts/MainLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header bordered style='background: #ffff'>
+      <q-toolbar >
+        <q-toolbar-title style='color: black; font-weight: 800; font-size: 40px'>
+          <q-avatar class="q-mb-sm q-pa-none">
+            <img src="https://mb.cision.com/Public/12609/2739140/bcd11e7f17a1e50d_org.jpg">
+          </q-avatar>
+          Food & Co.
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+const linksData = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ Amica_projekt/amica-nettan/src/pages/Index.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-page class='column flex-center'>
+    <!-- <div v-if="foodData != null" v-for="(food, i) in foodData.foodData" :key="i" class='flex flex-center bg-secondary q-pa-md q-ma-md rounded-borders'>
+      <h7 class='q-ma-none q-mr-md'>{{food.food}}</h7>
+      <q-btn @click = "addfood(i)" round color='positive' icon='add' class='q-ma-sm' />
+      <q-btn @click = "revfood(i)" round color='negative' icon='remove' class='q-ma-sm'/>
+      <q-chip class='bg-white' style='font-size: 16px; font-weight:800'>{{food.quantity}}</q-chip>
+    </div> -->
+    <div class='flex flex-center bg-secondary q-pa-md q-ma-md rounded-borders' style="position: relative; width: 90%">
+      <h7 class='q-ma-none q-mr-md'>{{foodData.foodData.food1.food}}</h7>
+      <div class="flex flex-center">
+      <q-btn @click = "addfood('food1')" round color='positive' icon='add' class='q-ma-sm' />
+      <q-btn @click = "revfood('food1')" round color='negative' icon='remove' class='q-ma-sm'/>
+      <q-chip class='bg-white' style='font-size: 16px; font-weight:800'>{{foodData.foodData.food1.quantity}}</q-chip>
+      </div>
+    </div>
+    <div class='flex flex-center bg-secondary q-pa-md q-ma-md rounded-borders' style="position: relative; width: 90%">
+      <h7 class='q-ma-none q-mr-md'>{{foodData.foodData.food2.food}}</h7>
+      <div class="flex flex-center">
+      <q-btn @click = "addfood('food2')" round color='positive' icon='add' class='q-ma-sm' />
+      <q-btn @click = "revfood('food2')" round color='negative' icon='remove' class='q-ma-sm'/>
+      <q-chip class='bg-white' style='font-size: 16px; font-weight:800'>{{foodData.foodData.food2.quantity}}</q-chip>
+      </div>
+    </div>
+    <div class='flex flex-center bg-secondary q-pa-md q-ma-md rounded-borders' style="position: relative; width: 90%">
+      <h7 class='q-ma-none q-mr-md'>{{foodData.foodData.food3.food}}</h7>
+      <div class="flex flex-center">
+      <q-btn @click = "addfood('food3')" round color='positive' icon='add' class='q-ma-sm' />
+      <q-btn @click = "revfood('food3')" round color='negative' icon='remove' class='q-ma-sm'/>
+      <q-chip class='bg-white' style='font-size: 16px; font-weight:800'>{{foodData.foodData.food3.quantity}}</q-chip>
+      </div>
+    </div>
+    <div class='column flex-center bg-secondary q-pa-md q-ma-md rounded-borders' style="position: relative; width: 90%">
+      <h7 class='q-ma-none q-mr-md'>{{foodData.foodData.food4.food}}</h7>
+      <div class="flex flex-center">
+        <q-btn @click = "addfood('food4')" round color='positive' icon='add' class='q-ma-sm' />
+        <q-btn @click = "revfood('food4')" round color='negative' icon='remove' class='q-ma-sm'/>
+        <q-chip class='bg-white' style='font-size: 16px; font-weight:800'>{{foodData.foodData.food4.quantity}}</q-chip>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { db, increment, decrement } from '../boot/firebase'
+import { date } from 'quasar'
+export default {
+  name: 'PageIndex',
+  created() {
+     this.currentDate = date.formatDate(Date.now(), 'YYYY-MM-DD')
+     this.$bind('foodData', db.collection('weektest').doc(this.currentDate))
+  },
+  data () {
+    return {
+      currentDate: '',
+      foodData: null
+    }
+  },
+  methods: {
+    addfood (value) {
+      db.collection('weektest').doc(this.currentDate).update(`foodData.${value}.quantity`, increment)
+    },
+    revfood (value) {
+      db.collection('weektest').doc(this.currentDate).update(`foodData.${value}.quantity`, decrement)
+    }
+  }
+}
+
+</script>
+<style>
+h7{font-size:17px}
+</style>
