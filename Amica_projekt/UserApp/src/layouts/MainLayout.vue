f4761cf2f690ce86a2ea12c3a55ad865d2b06721
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2020-10-13 12:47:32 +0200

    update to users app

--- Amica_projekt/UserApp/src/layouts/MainLayout.vue
+++ Amica_projekt/UserApp/src/layouts/MainLayout.vue
@@ -2,8 +2,8 @@
   <q-layout view="lHh Lpr lFf">
     <q-header bordered style='background: #ffff'>
       <q-toolbar >
-        <q-toolbar-title style='color: black; font-weight: 800; font-size: 40px'>
-          <q-avatar class="q-mb-sm q-pa-none">
+        <q-toolbar-title style='color: black; font-weight: 800; font-size: 60px'>
+          <q-avatar class="q-mb-sm q-pa-none" style="font-size: 60px">
             <img src="https://mb.cision.com/Public/12609/2739140/bcd11e7f17a1e50d_org.jpg">
           </q-avatar>
           Food & Co.
