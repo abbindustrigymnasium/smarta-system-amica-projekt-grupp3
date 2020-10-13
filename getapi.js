8e8db9431e75980267a62d5ff875cb202aba01c1
Joakim Eriksson <43879119+abbjoaeri@users.noreply.github.com>
2020-10-13 14:38:20 +0200

    Delete getapi.js

--- getapi.js
+++
@@ -1,5 +0,0 @@
-var day = getDay();
-var month = getMonth();
-var year = getFullYear();
-console.log(day)
-var api = "https://foodandco.se/api/restaurant/menu/day?date=" + "&language=sv&restaurantPageId=188244"
